ffc3c3ccee80e7646c4168af1106325474db3af9
matteohx@gmail.com <matteohx@gmail.com>
2020-06-15 14:29:10 +0200

    Create a copy of prefab

---
+++ Editor/SpawnDoubleEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SpawnDouble))]
+public class SpawnDoubleEditor : Editor
+{
+	SpawnDouble _instance;
+	private void OnEnable()
+	{
+		_instance = (SpawnDouble)target;
+	}
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+		if (GUILayout.Button("Create Copy At Offset multiplied by time pressed"))
+		{
+			_instance.SpawnCopy();
+		}
+
+		if (GUILayout.Button("Reset compteur"))
+		{
+			_instance.ResetCpt();
+		}
+	}
+}
+
---
+++ Runtime/SpawnDouble.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDouble : MonoBehaviour
+{
+    public GameObject m_prefab;
+    
+    public int cpt = 0;
+    public Vector3 m_offsetForSpawn;
+
+    public void SpawnCopy()
+    {
+        cpt++;
+        SpawnPrefab(m_prefab, m_offsetForSpawn * cpt);
+    }
+
+    void SpawnPrefab(GameObject prefab, Vector3 offset)
+    {
+        Instantiate(prefab, transform.position + offset, Quaternion.identity);
+    }
+
+    public void ResetCpt()
+    {
+        cpt = 0;
+    }
+
+    void Reset()
+    {
+        m_prefab = this.gameObject;
+    }
+}
